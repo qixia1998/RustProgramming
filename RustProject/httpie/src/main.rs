847b65c5424fa3312c75c05036ed8c4dc74e8ce1
qixia1998 <qixia717138552@gmail.com>
2023-08-27 23:15:51 +0800

    updated httpie

--- RustProject/httpie/src/main.rs
+++ RustProject/httpie/src/main.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use anyhow::Result;
+use reqwest::Url;
+use std::str::FromStr;
+use anyhow::{anyhow, Result};
 
 // 定义 HTTPie 的 CLI 的主入口，它包含若干个子命令
 // 下面 /// 的注释是文档，clap 会将其作为 CLI 的帮助
@@ -36,9 +40,55 @@ struct Get {
 #[derive(Parser, Debug)]
 struct Post {
     /// HTTP 请求的 URL
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
     /// HTTP 请求的 body
-    body: Vec<String>,
+    #[clap(parse(try_from_str=parse_kv_pair))]
+    body: Vec<KvPair>,
+}
+
+/// 命令行中的 key=value 可以通过 parse_kv_pair 解析成 KvPair 结构
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+/// 当我们实现 FromStr trait 后，可以用 str.parse() 方法将字符串解析成 KvPair
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // 使用 = 进行 split，这会得到一个迭代器
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            // 从迭代器中取第一个结果作为 key，迭代器返回 Some(T)/None
+            // 我们将其转换成 Ok(T)/Err(E)，然后用 ? 处理错误
+            k: (split.next().ok_or_else(err)?).to_string(),
+            // 从迭代器中取第二个结果作为 value
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+/// 因为我们为 KvPair 实现了 FromStr，这里可以直接 s.parse() 得到 KvPair
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+#[derive(Parser, Debug)]
+struct Get {
+    /// HTTP 请求的 URL
+    #[clap(parse(try_from_str = parse_url))]
+    url: String,
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    // 这里我们仅仅检查一下 URL 是否合法
+    let _url: Url = s.parse()?;
+
+    Ok(s.into())
 }
 
 fn main() {
