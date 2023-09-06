e190d46fcb2021fe92216daa958b8cecd74a4fc7
qixia1998 <qixia717138552@gmail.com>
2023-09-06 23:47:55 +0800

    updated httpie

--- RustProject/httpie/src/main.rs
+++ RustProject/httpie/src/main.rs
@@ -1,15 +1,18 @@
-use clap::Parser;
-use anyhow::Result;
-use reqwest::Url;
-use std::str::FromStr;
 use anyhow::{anyhow, Result};
+use clap::Parser;
+use colored::*;
+use mime::Mime;
+use reqwest::{header, Client, Response, Url};
+use std::{collections::HashMap, str::FromStr};
+
+// 以下部分用于处理 CLI
 
 // 定义 HTTPie 的 CLI 的主入口，它包含若干个子命令
 // 下面 /// 的注释是文档，clap 会将其作为 CLI 的帮助
 
 /// A naive httpie implementation with Rust, can you imagine how easy it is?
 #[derive(Parser, Debug)]
-#[clap(version = "1.0", author = "Tyr Chen <tyr@chen.com>")]
+#[clap(version = "1.0", author = "qixia")]
 
 struct Opts {
     #[clap(subcommand)]
@@ -30,6 +33,7 @@ enum SubCommand {
 #[derive(Parser, Debug)]
 struct Get {
     /// HTTP 请求的 URL
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
 }
 
@@ -48,7 +52,7 @@ struct Post {
 }
 
 /// 命令行中的 key=value 可以通过 parse_kv_pair 解析成 KvPair 结构
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct KvPair {
     k: String,
     v: String,
@@ -77,13 +81,6 @@ fn parse_kv_pair(s: &str) -> Result<KvPair> {
     Ok(s.parse()?)
 }
 
-#[derive(Parser, Debug)]
-struct Get {
-    /// HTTP 请求的 URL
-    #[clap(parse(try_from_str = parse_url))]
-    url: String,
-}
-
 fn parse_url(s: &str) -> Result<String> {
     // 这里我们仅仅检查一下 URL 是否合法
     let _url: Url = s.parse()?;
@@ -91,7 +88,114 @@ fn parse_url(s: &str) -> Result<String> {
     Ok(s.into())
 }
 
-fn main() {
+/// 处理 get 子命令
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+/// 处理 post 子命令
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+
+    print!("\n");
+}
+
+/// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+        // 其它 mime type，我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+
+/// 打印整个响应
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+/// 将服务器返回的 content-type 解析成 Mime 类型
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+/// 程序的入口函数，因为在 HTTP 请求时我们使用了异步处理，所以这里引入 tokio
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
-    println!("{:?}", opts);
+    let mut headers = header::HeaderMap::new();
+    // 为我们的 HTTP 客户端添加一些缺省的 HTTP 头
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
+    };
+
+    Ok(result)
+}
+
+// 仅在 cargo test 时才编译
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abc").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://httpbin.org/post").is_ok());
+    }
+
+    #[test]
+    fn parse_kv_pair_works() {
+        assert!(parse_kv_pair("a").is_err());
+        assert_eq!(
+            parse_kv_pair("a=1").unwrap(),
+            KvPair {
+                k: "a".into(),
+                v: "1".into()
+            }
+        );
+
+        assert_eq!(
+            parse_kv_pair("b=").unwrap(),
+            KvPair {
+                k: "b".into(),
+                v: "".into()
+            }
+        );
+    }
 }
